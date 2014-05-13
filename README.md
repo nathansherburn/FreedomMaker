f4032e5d03583a6fe23dc944c1f9ce28fc7cb111
Nathan <nathansherburn@gmail.com>
2014-05-13 10:35:34 +1000

    Various updates and feature adds

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ FreedomMaker
 ============
 
 Free your academic journals by converting your PDF files to a high-quality, responsive mobile webiste.
+
+This software was created by Engineers Without Borders Australia (ewb.org.au) as part of the Open Journal Project Initiative (openjournalproject.org).
+
+
--- tpgui.py
+++ tpgui.py
@@ -2,7 +2,7 @@
 
 from Tkinter import *
 import tkMessageBox
-from tkFileDialog import askopenfilename      
+from tkFileDialog import askopenfilename, asksaveasfilename
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
@@ -23,6 +23,9 @@ text_file = ""
 text_buffer = ""
 text_current_line = ""
 
+# undo memory
+last_action = "nothing"
+
 # initialize line to first
 line_number = 1
 
@@ -32,7 +35,7 @@ def open_file():
 	global text_buffer
 	global line_number
 	line_number = 1
-	pdf_path = askopenfilename()
+	pdf_path = askopenfilename(multiple=1)
 	text_file = convert_pdf_to_txt(pdf_path)
 	text_file = text_file.replace("\n\n", "<double new line>")
 	text_file = text_file.replace("\n", "")
@@ -73,23 +76,51 @@ def convert_pdf_to_txt(path):
 
 # define functions for text parsing
 def keep_text():
+	last_action = "keep"
 	new_text.config(state=NORMAL)
 	new_text.insert(END, text_current_line)
 	new_text.config(state=DISABLED)
 	fetch_line()
 
 def ignore_text():
+	last_action = "ignore"
 	fetch_line()
+
+def insert_new_line():
+	last_action = "newline"
+	new_text.config(state=NORMAL)
+	new_text.insert(END, "\n")
+	new_text.config(state=DISABLED)
 
 def fetch_line():
 	global text_buffer
 	global line_number
 	global text_current_line
+	new_text.see(END)
 	text_current_line = text_buffer.readline()
-	original_text.tag_remove("highlight", "1.0", END)
-	original_text.tag_add("highlight", str(line_number)+".0", str(line_number)+"."+str(len(text_current_line)))
-	original_text.tag_config("highlight", background="yellow", foreground="black")
-	line_number += 1
+	if len(text_current_line) > 1:
+		original_text.tag_remove("highlight", "1.0", END)
+		original_text.tag_add("highlight", str(line_number)+".0", str(line_number)+"."+str(len(text_current_line)))
+		original_text.tag_config("highlight", background="yellow", foreground="black")
+		line_number += 1
+	else:
+		line_number += 1
+		fetch_line()
+
+def undo():
+	pass
+
+def create_website():
+	filename = asksaveasfilename()
+	if filename:
+		web_content = new_text.get(1.0, END)
+		web_content = web_content.replace("\n", "<br>")
+		save_file_fp = open(filename+".html", "w")
+		save_file_fp.write("<!DOCTYPE HTML>\n<html><header></header><body>\n")
+		save_file_fp.write(web_content)
+		save_file_fp.write("</body>\n</html>")
+		save_file_fp.close()
+
 
 # define the menu at the window top
 menubar = Menu(main_window)
@@ -129,20 +160,25 @@ keep_btn.pack()
 ignore_btn = Button(main_window, width=12, text="Ignore", command=ignore_text)
 ignore_btn.pack()
 
-create_btn = Button(main_window, width=12, text="Create Website", command=do_nothing)
-create_btn.pack()
+newl_btn = Button(main_window, width=12, text="New Line", command=insert_new_line)
+newl_btn.pack()
 
+undo_btn = Button(main_window, width=12, text="Undo", command=undo)
+undo_btn.pack()
+
+create_btn = Button(main_window, width=12, text="Create Website", command=create_website)
+create_btn.pack()
 
 # define textboxes
 original_text = Text(main_window)
 original_text.insert(INSERT, "Click 'File' > 'Open PDF' to begin...")
-original_text.pack()
 original_text.place(anchor=SW, relheight=1.0, relwidth=0.4, relx=0.0, rely=1.0)
+original_text.config(state=DISABLED)
 
 new_text = Text(main_window)
 new_text.insert(INSERT, "This is where your good content will go...")
-new_text.pack()
 new_text.place(anchor=SE, relheight=1.0, relwidth=0.4, relx=1.0, rely=1.0, x=-12)
+new_text.config(state=DISABLED)
 
 
 # define the scrollbars
@@ -150,12 +186,10 @@ original_text_scrollbar = Scrollbar(main_window)
 original_text_scrollbar.config(command=original_text.yview)
 original_text.config(yscrollcommand=original_text_scrollbar.set)
 original_text_scrollbar.place(relheight=1.0, relx=0.4)
-original_text.config(state=DISABLED)
 
 new_text_scrollbar = Scrollbar(main_window)
 new_text_scrollbar.config(command=new_text.yview)
 new_text.config(yscrollcommand=new_text_scrollbar.set)
 new_text_scrollbar.place(relheight=1.0, relx=1.0, x=-12)
-new_text.config(state=DISABLED)
 
 main_window.mainloop()
